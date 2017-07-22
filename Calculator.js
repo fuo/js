2759119cdf2c59838c2373cb157e0dda6b4e3216
fuo <pha.m@icloud.com>
2017-07-23 00:13:47 +0700

    clean v1

--- Calculator.js
+++ Calculator.js
@@ -4,19 +4,19 @@ class Calculator {
         var calc_itself = this;    
 		calc_itself.state = {
 	  	    input: '',
-            cachedInput: '',
+            result: '',
 		    accumulator: 0.0,      
             lastOperator: function() {
                 var state = this;   
-      	        state.accumulator = state.input;
+      	        return state.accumulator = state.input;
             }
 	    };
 
   };
   
   pressNumeric(value) {
-  	this.state.input = `${this.state.input}${value.match(/^[0-9]$/)}`;
-    return this.state.cachedInput = this.state.input;
+  	this.state.input = this.state.result.toString().split('.').length > 1 ? `${this.state.input}${value.match(/^[0-9]$/)}` : parseFloat(`${this.state.input}${value.match(/^[0-9]$/)}`);
+    return this.state.result = this.state.input;
   };
 
   pressDot() {
@@ -25,57 +25,54 @@ class Calculator {
 	  .replace(/\./, "x")
 	  .replace(/\./g, "")
 	  .replace(/x/, ".");
-	this.state.cachedInput = this.state.input;
+	this.state.result = this.state.input;
   };
 
   pressMultiply() {
   	this.execute();
     this.state.lastOperator = function() {
-    	this.accumulator = (this.accumulator || 0) * (this.input || 1);
+    	return this.accumulator = (this.accumulator || 0) * (this.input || 1);
     };
   };
   
   pressAdd() {
   	this.execute();
     this.state.lastOperator = function() {
-    	this.accumulator = (this.accumulator || 0) + (this.input || 0);
+    	return this.accumulator = (this.accumulator || 0) - (-this.input || 0);
     };
   };
   
   pressSubtract() {
   	this.execute();
     this.state.lastOperator = function() {
-    	this.accumulator = (this.accumulator || 0) - (this.input || 0);
+    	return this.accumulator = (this.accumulator || 0) - (this.input || 0);
     };
   };
   
   pressDivide() {
   	this.execute();
     this.state.lastOperator = function() {
-    	this.accumulator = (this.accumulator || 0) / (this.input || 1);
+    	return this.accumulator = (this.accumulator || 0) / (this.input || 1);
     };
   };
   
   pressEqual() {
   	this.execute();
     this.state.lastOperator = function() {
-    	this.accumulator = this.cachedInput;
+    	return this.accumulator = this.result;
     };
   };
   
   onPress(id) {
     var func = 'press' + id.charAt(0).toUpperCase() + id.slice(1);
     this[func]();
-	return this.state.cachedInput;
+	return this.state.result;
   };
 
   execute() {
-	this.state.input = parseFloat(this.state.input);
-  	this.state.lastOperator();
-    this.state.cachedInput = this.state.accumulator;
+	this.state.input = parseFloat(this.state.input || 0);
+    this.state.result = this.state.lastOperator();
     this.state.input = '';
-  };
-  
-	getState() { return this.state; };
+  };  
   
 }
