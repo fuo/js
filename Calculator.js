e1f541b7e7462afce49688d5a867dc82dd1d41c9
fuo <pha.m@icloud.com>
2017-07-26 11:30:32 +0700

    clean v1.2

--- Calculator.js
+++ Calculator.js
@@ -1,77 +1,76 @@
 class Calculator {
     
   constructor() {
-    var calc_itself = this;    
-	calc_itself.state = {
+	this.state = {
 	    input: '',
-	    result: '',
 	    accumulator: 0.0,      
         lastOperator: function() {
             var state = this;   
   	        return state.accumulator = state.input;
-        }
+        },
+   	    execute: function() {
+		 	var state = this;
+			state.input = parseFloat(state.input || 0);
+	    	state.result = state.lastOperator();
+	    	state.input = '';
+	    }
     };
   };
   
   pressNumeric(value) {
-  	this.state.input = this.state.result.toString().split('.').length > 1 ? `${this.state.input}${value.match(/^[0-9]$/)}` : parseFloat(`${this.state.input}${value.match(/^[0-9]$/)}`);
-    return this.state.result = this.state.input;
+  	return this.state.input = this.state.input.toString().split('.').length > 1 ? `${this.state.input}${value.match(/^[0-9]$/)}` : parseFloat(`${this.state.input}${value.match(/^[0-9]$/)}`);
   };
 
   pressDot() {
-	this.state.input = this.state.input || '0';
-    this.state.input = (this.state.input + '.').replace(/[^\d\.]/g, "")
+	return this.state.input = (this.state.input || '0' + '.').replace(/[^\d\.]/g, "")
 	  .replace(/\./, "x")
 	  .replace(/\./g, "")
 	  .replace(/x/, ".");
-	this.state.result = this.state.input;
   };
 
   pressMultiply() {
   	this.execute();
     this.state.lastOperator = function() {
-    	return this.accumulator = (this.accumulator || 0) * (this.input || 1);
+    	return this.accumulator *= this.input || 1;
     };
   };
   
   pressAdd() {
   	this.execute();
     this.state.lastOperator = function() {
-    	return this.accumulator = (this.accumulator || 0) - (-this.input || 0);
+    	return this.accumulator -= -this.input || 0;
     };
   };
   
   pressSubtract() {
   	this.execute();
     this.state.lastOperator = function() {
-    	return this.accumulator = (this.accumulator || 0) - (this.input || 0);
+    	return this.accumulator -=  this.input || 0;
     };
   };
   
   pressDivide() {
   	this.execute();
     this.state.lastOperator = function() {
-    	return this.accumulator = (this.accumulator || 0) / (this.input || 1);
+    	return this.accumulator /= this.input || 1;
     };
   };
   
   pressEqual() {
   	this.execute();
     this.state.lastOperator = function() {
-    	return this.accumulator = this.result;
+    	return this.accumulator;
     };
   };
   
   onPress(id) {
     var func = 'press' + id.charAt(0).toUpperCase() + id.slice(1);
     this[func]();
-	return this.state.result;
+	return this.state.accumulator;
   };
 
   execute() {
-	this.state.input = parseFloat(this.state.input || 0);
-    this.state.result = this.state.lastOperator();
-    this.state.input = '';
+	this.state.execute();
   };  
   
 }
