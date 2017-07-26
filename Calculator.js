24c0a7c6479512bb19bdf99dd17bb0684b0527df
Phuong Pham <pha.m@icloud.com>
2017-07-26 22:50:54 +0700

    tidy up

--- Calculator.js
+++ Calculator.js
@@ -2,19 +2,20 @@ class Calculator {
 
   constructor() {
     var calc = this
-    calc.display = '0'
     calc.state = {
       calc: calc,
+      display: '0',
       input: '',
       accumulator: 0.0,
       lastOperator: function() {
         var state = this;
-        return state.accumulator = state.input;
+        state.accumulator = state.input;
       },
       execute: function() {
         var state = this;
         state.input = parseFloat(state.input || 0);
         state.lastOperator();
+        state.display = state.accumulator;
         state.input = '';
       }
     };
@@ -22,7 +23,7 @@ class Calculator {
 
   pressNumeric(value) {
     this.state.input = this.state.input.toString().split('.').length > 1 ? `${this.state.input}${value.match(/^[0-9]$/)}` : parseFloat(`${this.state.input}${value.match(/^[0-9]$/)}`);
-    return this.display = this.state.input
+    this.state.display = this.state.input
   };
 
   pressDot() {
@@ -30,54 +31,56 @@ class Calculator {
       .replace(/\./, "x")
       .replace(/\./g, "")
       .replace(/x/, ".");
-    return this.display = this.state.input;
+    this.state.display = this.state.input;
   };
 
   pressMultiply() {
     this.execute();
     this.state.lastOperator = function() {
-      return this.accumulator *= this.input || 1;
+      this.accumulator *= this.input || 1;
     };
-    return this.display = this.state.accumulator
   };
 
   pressAdd() {
     this.execute();
     this.state.lastOperator = function() {
-      return this.accumulator -= -this.input || 0;
+      this.accumulator -= -this.input || 0;
     };
-    return this.display = this.state.accumulator
+
   };
 
   pressSubtract() {
     this.execute();
     this.state.lastOperator = function() {
-      return this.accumulator -= this.input || 0;
+      this.accumulator -= this.input || 0;
     };
-    return this.display = this.state.accumulator
+
   };
 
   pressDivide() {
     this.execute();
     this.state.lastOperator = function() {
-      return this.accumulator /= this.input || 1;
+      this.accumulator /= this.input || 1;
     };
-    return this.display = this.state.accumulator
+
   };
 
   pressEqual() {
     this.execute();
     this.state.lastOperator = function() {
-      return this.accumulator = calc.display;
+      this.accumulator = this.display;
     };
-    return this.display = this.state.accumulator
+
   };
 
   onPress(id) {
     var func = 'press' + id.charAt(0).toUpperCase() + id.slice(1);
-    return this[func]();
-
+    this[func]();
   };
+
+  display() {
+    return this.state.display;
+  }
 
   execute() {
     this.state.execute();
